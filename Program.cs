b1d34d3f7c5fb7cb20f5137aacb074a52b768904
Anton Sopov <recorrd@yandex.ru>
2024-01-21 22:09:05 +0300

    Add Task3 to Task3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿ 
+Console.Write("Введите длину массива: ");
+int N = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[N];
+Random random = new Random(); // Генератор случайных чисел
+Console.Write("Исходный массив: ");
+for (int i = 0; i < numbers.Length; i++)
+{
+numbers[i] = random.Next(1, 100); 
+Console.Write(numbers[i] + " ");
+}
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+// Меняем местами элементы
+int temp = numbers[i];
+numbers[i] = numbers[numbers.Length - 1 - i];
+numbers[numbers.Length - 1 - i] = temp;
+}
+// Вывод измененного массива
+Console.Write("\nПеревернутый массив: ");
+foreach (int number in numbers)
+{
+Console.Write(number + " ");
+}
