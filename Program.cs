c4cfa98f5d0ecba532e672717b1400a954a3c635
Anton Sopov <recorrd@yandex.ru>
2024-01-21 21:41:57 +0300

    Add Task2 to Task2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int[] numbers = new int[10]; 
+Random random = new Random(); // Генератор случайных чисел
+int evenCount = 0; 
+for (int i = 0; i < numbers.Length; i++)
+{
+numbers[i] = random.Next(100, 1000); 
+Console.Write(numbers[i] + " "); 
+// Проверка на четность и увеличение счетчика
+if (numbers[i] % 2 == 0)
+{
+evenCount++;
+}
+}
+// Вывод количества четных чисел в массиве
+Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
